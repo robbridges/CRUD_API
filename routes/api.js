519d9341dd2832a0f18e3711e4e3898528a11e54
robbridges <robbridges@users.noreply.github.com>
2021-03-19 21:23:20 -0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -20,8 +20,8 @@ router.get('/courses', asyncHandler(async (req, res) => {
 // adds user to the database
 router.post('/users', asyncHandler(async (req, res) => {
   try {
-  let user = await User.create(req.body);
-  res.json(user);
+    let user = await User.create(req.body);
+    res.json(user);
   } catch (error) {
     res.json(error);
   }
