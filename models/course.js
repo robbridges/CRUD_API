b853bfdabad2e9a04347391f4fce596288324fdc
robbridges <robbridges@users.noreply.github.com>
2021-03-19 18:56:05 -0700

    more fixes

--- models/course.js
+++ models/course.js
@@ -19,5 +19,6 @@ module.exports =(sequelize, DataTypes) => {
       fieldName: 'userId',
     });
   }
+  
   return Course;
 }
--- models/user.js
+++ models/user.js
@@ -19,5 +19,6 @@ module.exports = (sequelize, DataTypes) => {
       foreignKey: 'userId',
     });
   }
+  
   return User;
 }
