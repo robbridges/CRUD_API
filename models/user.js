41b30ca82536d1f109ca76bb89cae4b92e69a70b
robbridges <robbridges@users.noreply.github.com>
2021-03-20 18:08:50 -0700

    user auth seems to be working

--- models/user.js
+++ models/user.js
@@ -1,14 +1,19 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
+const bcrypt = require('bcryptjs');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {};
   User.init({
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
     emailAddress: DataTypes.STRING,
-    password: DataTypes.STRING,
+    password: {
+      type: DataTypes.STRING,
+      set(val) {
+        const hash = bcrypt.hashSync(val, 10);
+        this.setDataValue('password', hash);
+      }
+    } 
   }, {
     sequelize,
     modelName: 'User',
